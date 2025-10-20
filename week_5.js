f3e8832ca4911f033fd1405281850896039e1440
YusufKumo <engrysk77@gmail.com>
2025-10-20 16:19:04 +0100

    Add files via upload

---
+++ week_5.js
@@ -0,0 +1,20 @@
+<body>
+<script>
+const hospital = {name:"Kumo Medical Clinic", location:"Kumo", address:"along Gombe to Yola road",
+ serviceHours:"24 Hours"
+}
+
+document.getElementById("yusmin").innerHTML = hospital.name + " is located in " + hospital.location + " " + hospital.address + " with " + hospital.serviceHours + ".";
+
+
+let m = 3
+let n = 3
+let x=m+n
+documet.getElementById('yusmin').innerText = x;
+</script>
+
+<p> I tried all other means to show the rest of the learning outcome; objects, function, comparisons, booleans etc. but unfortunately,
+    they're not displaying despite watching several videos on how to merge multiple JavaScript functions in one script, <strong> I need a mentor to guide me</strong>.
+</p>
+
+</body>
